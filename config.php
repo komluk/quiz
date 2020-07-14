f55cd8c3719a582c0a0868b32a76bcc99d855992
lukasz.komosa <lukasz.komosa@komluk.com>
2020-07-14 15:46:55 +0200

    index.html

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Basic configuration of variables for including files
-define('ROOT_PATH', dirname(__FILE__));
-define('HOST_PATH', 'http://localhost/quiz/');
-
-//--------- Define database user and password
-define('DB_USER', 'root');
-define('DB_PASS', '');
-?>
