af8ef549656128fb0a8c720d1ddc9e8870f1e3ab
lukasz.komosa <lukasz.komosa@komluk.com>
2020-07-14 14:53:48 +0200

    initial code

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Basic configuration of variables for including files
+define('ROOT_PATH', dirname(__FILE__));
+define('HOST_PATH', 'http://localhost/quiz/');
+
+//--------- Define database user and password
+define('DB_USER', 'root');
+define('DB_PASS', '');
+?>
---
+++ src/index.php
@@ -0,0 +1,37 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Quizzer!</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <header>
+        <div class="container">
+          <h1>PHP Quizzer</h1>
+	</div>
+      </header>
+
+
+      <main>
+      <div class="container">
+        <h2>Test your PHP Knowledge</h2>
+	<p>This is a multiple choice quize to test your knowledge about something</p>
+    <ul>
+    </ul>
+	<a>Start Quiz</a>
+      </div>
+    </div>
+    </main>
+
+
+    <footer>
+      <div class="container">
+      	   Copyright &copy; 2020
+      </div>
+    </footer>
+  </body>
+</html>
