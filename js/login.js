a8f32357dec53f2db257678058a221e8cdb20e6a
lukasz.komosa <lukasz.komosa@komluk.com>
2020-07-17 16:02:00 +0200

    quiz ui

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const username = document.getElementById('name');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+
+username.addEventListener('keyup', () => {
+    loginBtn.disabled = !username.value || !password.value;
+});
+
+password.addEventListener('keyup', () => {
+    loginBtn.disabled = !username.value || !password.value;
+});
+
+login = (e) => {
+    e.preventDefault();
+    alert('Register!');    
+
+    window.location.assign('/quiz');
+}
